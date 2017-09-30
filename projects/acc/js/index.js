1b9ed0b99afd21058a3f598b323f727a63c8fda6
Girish Rawat <Emailgirishrawat@gmail.com>
2017-09-30 07:48:59 -0700

    removed the front table

--- projects/acc/js/index.js
+++ projects/acc/js/index.js
@@ -46,11 +46,6 @@ function accelerationHandler(acceleration, targetId) {
 function MaccelerationHandler(acceleration, targetId) {
   var info, xyz = "[X, Y, Z]";
 
-  info = xyz.replace("X", acceleration.x && acceleration.x.toFixed(3));
-  info = info.replace("Y", acceleration.y && acceleration.y.toFixed(3));
-  info = info.replace("Z", acceleration.z && acceleration.z.toFixed(3));
-  document.getElementById(targetId).innerHTML = info;
-
   var zz = (acceleration.z && acceleration.z.toFixed(3));
 
   if (zz>9.8) {
