76744d12249c9b1545795cdacbe4a78697af61ce
Girish Rawat <Emailgirishrawat@gmail.com>
2017-09-30 22:57:10 -0700

    ffff

--- pong2/acc/blue/js/index.js
+++ pong2/acc/blue/js/index.js
@@ -58,13 +58,13 @@ function MaccelerationHandler(acceleration, targetId) {
                   color = "#000000";
                   x = 1;
               }
-
-              document.body.style.background = "#000000";
             }, 50);
 
           setTimeout(function() { 
             clearInterval(timer); 
           }, 1000); 
+
+          document.body.style.background = "#000000";
   }
 }
 
